0e4c2ae2e15457144cfc087fd9395821b6e2ee9c
qiutingli <qiutingli.china@gmail.com>
2020-05-14 12:09:56 +1000

    Add presenting standard deviation, covariance, and correlation coefficient functionality.

--- main/data_summary.py
+++ main/data_summary.py
@@ -4,14 +4,15 @@ import re
 import datetime
 import logging
 import matplotlib.pyplot as plt
-from statistics import mean, median
+import statistics
+import numpy as np
 from data_loader import DataLoader
 
 
 def _setup_logger():
     global logger
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        filename='logginng')
+                        filename='{}/log/log.txt'.format(os.path.pardir))
     logger = logging.getLogger(__name__)
 
 
@@ -26,8 +27,13 @@ class SummaryGenerater():
         for title in self.athlete_dataframe['Title'].value_counts().index:
             print(tuple(self.athlete_dataframe[self.athlete_dataframe['Title'] == title][['Title', 'Activity Type']].iloc[0]))
         print('\n')
-        print('====== Activity Type and Counts ======')
+        print('====== Activity Types and Counts ======')
         print(self.athlete_dataframe['Activity Type'].value_counts())
+
+    def summarize_dataset_shape(self):
+        print('====== Shape of the Dataset ======')
+        df_shape = self.athlete_dataframe.shape
+        print('The dataset has {} rows and {} columns'.format(df_shape[0], df_shape[1]))
 
     def summarize_dates(self, activity_type):
         '''
@@ -56,8 +62,9 @@ class SummaryGenerater():
                     total_seconds_list.append(total_seconds)
             print('Max workout time: {} seconds'.format(max(total_seconds_list)))
             print('Min workout time: {} seconds'.format(min(total_seconds_list)))
-            print('Mean workout time: {} seconds'.format(round(mean(total_seconds_list)), 2))
-            print('Median workout time: {} seconds'.format(median(total_seconds_list)))
+            print('Mean workout time: {} seconds'.format(round(statistics.mean(total_seconds_list)), 2))
+            print('Median workout time: {} seconds'.format(statistics.median(total_seconds_list)))
+            print('Standard deviation of workout time: {}'.format(np.std(total_seconds_list)))
             # plt.boxplot(total_seconds_list)
             # plt.show()
         except Exception as e:
@@ -73,16 +80,27 @@ class SummaryGenerater():
                     print('--- {} Summary ---'.format(feature))
                     print('Max {}: {}'.format(feature, max(feature_values)))
                     print('Min {}: {}'.format(feature, min(feature_values)))
-                    print('Mean {}: {}'.format(feature, round(mean(feature_values)), 2))
-                    print('Median {}: {}'.format(feature, median(feature_values)))
+                    print('Mean {}: {}'.format(feature, round(statistics.mean(feature_values)), 2))
+                    print('Median {}: {}'.format(feature, statistics.median(feature_values)))
+                    print('Standard deviation of {}: {}'.format(feature, np.std(feature_values)))
+                    print('Covariance for {} and TSS: {}'.format(feature, np.cov(feature_values, df['Training Stress Score®'])))
+                    print('Correlation for {} and TSS: {}'.format(feature, np.corrcoef(feature_values, df['Training Stress Score®'])))
             except Exception as e:
                 logger.exception('FILE: {}'.format(self.file_name), e)
+
+    def summarize_unique_features(self):
+        print(self.file_name)
+        print(self.athlete_dataframe.columns)
+
+    def summatize_feature_correlations(self):
+        pass
 
 
 def _process_summarizing(file_name):
     athlete_dataframe = DataLoader(file_name).load_athlete_dataframe()
     generator = SummaryGenerater(file_name, athlete_dataframe)
     generator.summarize_sport_categories()
+    generator.summarize_dataset_shape()
     print('\n')
     activity_types = generator.athlete_dataframe['Activity Type'].value_counts().index[0:4]
     for activity_type in activity_types:
@@ -97,6 +115,8 @@ def _process_summarizing(file_name):
 
 if __name__ == '__main__':
     _setup_logger()
+    log_file = open('{}/log/log.txt'.format(os.path.pardir), 'w')
+    sys.stdout = log_file
     data_path = '{}/data'.format(os.path.pardir)
     dirs = os.listdir(data_path)
     for file_name in dirs:
@@ -104,6 +124,7 @@ if __name__ == '__main__':
             sys.stdout = open('{}/data_summaries/{} Summary.txt'.format(os.path.pardir, file_name), 'w')
             _process_summarizing(file_name)
             sys.stdout.close()
+    sys.stdout.close()
 
 
 
