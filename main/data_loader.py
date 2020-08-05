6160dc9fc86a352b290e780074ad0b5b10a80074
qiutingli <qiutingli.china@gmail.com>
2020-08-05 11:50:43 +1000

    Add comments

--- main/data_loader.py
+++ main/data_loader.py
@@ -1,3 +1,19 @@
+"""Athletes Data Loader
+
+This script allows the user to load the athletes data.
+
+This tool accepts comma separated value files (.csv).
+
+This script requires that `pandas` be installed within the Python
+environment you are running this script in.
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * load_athlete_dataframe - returns a pandas data_frame
+    * main - the main function of the script
+"""
+
 import os
 import pandas as pd
 
