799d8205f99a09d9444ac25b5224dd578e5f7298
qiutingli <qiutingli.china@gmail.com>
2020-08-06 19:12:05 +1000

    Update comments

--- main/data_cleaning.py
+++ main/data_cleaning.py
@@ -8,6 +8,8 @@ environment you are running this script in.
 This file can also be imported as a module
 """
 
+import pandas as pd
+
 
 class CoachingMateDataCleaner():
     """
@@ -22,10 +24,12 @@ class CoachingMateDataCleaner():
 
     Methods
     -------
-    replace_miss_vals_with_nan()
+    replace_missing_vals_with_nan()
        Replace different kinds of missing values with Nah
     formalize_dates()
         Formalize the dates to YYYY-mm-dd HH:MM:SS form
+    process_data_cleaning()
+        Process the data cleaning
     """
 
     def __init__(self, file_name):
@@ -43,22 +47,51 @@ class CoachingMateDataCleaner():
     def make_correlation_plot(self):
         pass
 
-    def replace_miss_vals_with_nan(self):
+    def replace_missing_vals_with_nan(self):
         pass
 
     def formalize_dates(self):
         pass
 
     def process_data_cleaning(self):
+        """Process the data cleaning
+
+        Returns
+        -------
+        cleaned_df : pandas data frame
+            Cleaned athlete CoachingMate data
+        """
         pass
 
 
 class GarminDataCleaner():
+    """
+    A class used to process data cleaning on a group of Garmin datasets for one athlete
+
+    ...
+
+    Attributes
+    ----------
+    dir_name : str
+      The name of the directory at where Garmin data for a certain athlete located
+
+    Methods
+    -------
+    process_data_cleaning()
+       Process the data cleaning
+    """
 
     def __init__(self, dir_name):
         self.dir_name = dir_name
 
     def process_data_cleaning(self):
+        """Process the data cleaning
+
+        Returns
+        -------
+        cleaned_df : pandas data frame
+            Cleaned athlete Garmin data
+        """
         pass
 
 
