82b2f9f50b5f97b89c41c8c9b0a2f648ff63911b
qiutingli <qiutingli.china@gmail.com>
2020-08-12 15:32:12 +1000

    Add feature engineering template

---
+++ main/data_feature_engineering.py
@@ -0,0 +1,31 @@
+"""Data feature engineering
+
+This script allows the user to do feature engineering on the athletes data.
+
+This tool accepts comma separated value files (.csv).
+
+This script requires that `pandas` be installed within the Python
+environment you are running this script in.
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * load_athlete_dataframe - returns a pandas data_frame
+    * main - the main function of the script
+"""
+
+class OriginalDataFeatureExtractor():
+
+    def __init__(self):
+        pass
+
+
+class AdditionalDataFeatureExtractor():
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
