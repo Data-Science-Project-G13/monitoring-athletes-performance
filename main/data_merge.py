dd3fb3754f804dc874288980ab471368d0df6325
qiutingli <qiutingli.china@gmail.com>
2020-09-10 14:18:52 +1000

    Implement data merge

--- main/data_merge.py
+++ main/data_merge.py
@@ -1,4 +1,8 @@
+import pandas as pd
 import data_feature_engineering
+
+pd.set_option('display.max_row', 200)
+pd.set_option('display.max_columns', 10)
 
 def merge_spreadsheet_additional(athletes_name):
     """Merge the spreadsheet data and the additional data
@@ -6,11 +10,11 @@ def merge_spreadsheet_additional(athletes_name):
     spreadsheet = data_feature_engineering.main('spreadsheet', athletes_name)
     additionals = data_feature_engineering.main('additional', athletes_name)
 
-    for add in additionals:
-        pass
     for index, record in spreadsheet.iterrows():
-        print(record['Date'].split(' ')[0])
-        break
+        date = record['Date'].split(' ')[0]
+        if date in additionals.keys():
+            spreadsheet.at[index, 'Training Stress Score®'] = additionals[date]['TSS']
+    print(list(spreadsheet['Training Stress Score®']))
 
 
 if __name__ == '__main__':
