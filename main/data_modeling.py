f1d16d6d3f7aeb81bdb5388de4c5a5cb54ad0058
qiutingli <qiutingli.china@gmail.com>
2020-09-16 05:45:10 +1000

    Add Random Forest, SVM, Neural Network baselines

--- main/data_modeling.py
+++ main/data_modeling.py
@@ -1,13 +1,17 @@
 # Packages
 import numpy as np
 import pandas as pd
+from tensorflow.keras import Sequential, layers
+from tensorflow.keras.utils import to_categorical
 from sklearn import svm
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import precision_recall_curve, average_precision_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.preprocessing import LabelEncoder
 # Self-defined modules
 import utility
 import data_loader
+
 
 
 # Set the data frame display option
@@ -19,41 +23,78 @@ class TrainLoadModelBuilder():
 
     def __init__(self, dataframe):
         features = ['Distance', 'Calories', 'Avg HR', 'Max HR',
-                    'Training Stress Score®', 'Num Uniq Acts Weekly', 'Duration',
+                    'Training Stress Score®', 'Num Uniq Acts Weekly', 'Duration', 'ROLL TSS SUM',
                     'Activity Code 0', 'Activity Code 1', 'Activity Code 2', 'Activity Code 3', 'Activity Code 4',
                     'Normalized Power® (NP®)', 'Max Avg Power (20 min)', 'Avg Power', 'Max Power']
+        self.num_features = len(features)
         self.X = dataframe[features]
         self.y = dataframe['Training Load Indicator']
-        self.random_state = np.random.RandomState(0)
 
     def _split_train_validation(self):
         X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size = 0.2, random_state = 25)
         return X_train, X_test, y_train, y_test
 
-    def _process_random_forest(self):
-        pass
+    def _process_neural_network(self, X_train, X_test, y_train, y_test):
+        neural_network = Sequential()
+        verbose, epochs, batch_size = 0, 30, 4
+        neural_network.add(layers.Dense(265, input_shape=(self.num_features,), activation='relu'))
+        neural_network.add(layers.BatchNormalization())
+        neural_network.add(layers.Dense(64, activation='relu'))
+        neural_network.add(layers.Dense(1, activation='sigmoid'))
+        neural_network.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+        neural_network.fit(X_train, y_train,  validation_data=(X_test, y_test),
+                           epochs=epochs, batch_size=batch_size, shuffle=True)
+        # TODO: Add Grid Search
+        train_acc = neural_network.evaluate(X_train, y_train, verbose=0)[1]
+        test_acc = neural_network.evaluate(X_test, y_test, verbose=0)[1]
+        print('Training Set Accuracy: {}, Test Set Accuracy: {}'.format(train_acc, test_acc))
+        return neural_network
+
+    def _process_random_forest(self, X_train, y_train):
+        rfc = RandomForestClassifier(max_depth=3, random_state=0)
+        rfc.fit(X_train, y_train)
+        return rfc
 
     def _process_svm_classification(self, X_train, y_train):
-        classifier = svm.LinearSVC(random_state=self.random_state)
+        classifier = svm.SVC(decision_function_shape='ovo')
         classifier.fit(X_train, y_train)
         return classifier
 
-    def _validate_model(self, X_test, y_test, classifier: svm):
-        accuracy, precision, recall, f1_score = 0, 0, 0, 0
-        y_score = classifier.decision_function(X_test)
-        n_classes = 3
-        precision, recall, average_precision = dict(), dict(), dict()
-        for i in range(n_classes):
-            precision[i], recall[i], _ = precision_recall_curve(y_test[:, i], y_score[:, i])
-            average_precision[i] = average_precision_score(y_test[:, i], y_score[:, i])
-        return precision, recall, average_precision
+    def _validate_model(self, X_test, y_test, classifier):
+        y_preds = classifier.predict(X_test)  # predict classes for y test
+        print(y_preds)
+        accuracy = accuracy_score(y_test, y_preds)
+        precision = precision_score(y_test, y_preds, average='macro', zero_division=0)
+        recall = recall_score(y_test, y_preds, average='macro')
+        f1 = f1_score(y_test, y_preds, average='macro')
+        # auc = roc_auc_score(y_test, y_pred_probs)
+        # y_score = classifier.decision_function(X_test)
+        # n_classes = 3
+        # precision, recall, average_precision = dict(), dict(), dict()
+        # for i in range(n_classes):
+        #     precision[i], recall[i], _ = precision_recall_curve(y_test[:, i], y_score[:, i])
+        #     average_precision[i] = average_precision_score(y_test[:, i], y_score[:, i])
+        return accuracy, precision, recall, f1
+
+    def _display_performance_results(self, model_name, accuracy, precision, recall, f1):
+        print('Classifier: {}'.format(model_name))
+        print('Accuracy: {}, Precision: {}, Recall: {}, F1 score: {}'
+              .format(round(accuracy, 2), round(precision, 2), round(recall, 2), round(f1, 2)))
 
     def process_modeling(self):
         X_train, X_test, y_train, y_test = self._split_train_validation()
         print('X-y train-test shapes', X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+        # # ============ Random Forest ============
+        classifier = self._process_random_forest(X_train, y_train)
+        accuracy, precision, recall, f1 = self._validate_model(X_test, y_test, classifier)
+        self._display_performance_results('Random Forest', accuracy, precision, recall, f1)
+        # # ============ SVM ============
+        classifier = self._process_svm_classification(X_train, y_train)
+        accuracy, precision, recall, f1 = self._validate_model(X_test, y_test, classifier)
+        self._display_performance_results('SVM', accuracy, precision, recall, f1)
+        # ============ Neural Network ============
+        classifier = self._process_neural_network(X_train, X_test, y_train, y_test)
 
-        # classifier = self._process_svm_classification(X_train, y_train)
-        # precision, recall, average_precision = self._validate_model(X_test, y_test, classifier)
 
 
 class PerformanceModelBuilder():
