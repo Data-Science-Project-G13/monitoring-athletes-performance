6124bd300353eb75d223cc745eae5a88e3197e72
qiutingli <qiutingli.china@gmail.com>
2020-09-16 00:57:02 +1000

    Add modeling structure

--- main/data_modeling.py
+++ main/data_modeling.py
@@ -1,18 +1,28 @@
 # Packages
 import numpy as np
+import pandas as pd
 from sklearn import svm
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_recall_curve, average_precision_score
 # Self-defined modules
 import utility
+import data_loader
+
+
+# Set the data frame display option
+pd.set_option('display.max_row', 20)
+pd.set_option('display.max_columns', 20)
 
 
 class TrainLoadModelBuilder():
 
     def __init__(self, dataframe):
+        features = ['Distance', 'Duration', 'Calories', 'Avg HR', 'Max HR', 'Avg Speed', 'Max Speed',
+                    'Training Stress Score®',
+                    'Normalized Power® (NP®)', 'Max Avg Power (20 min)', 'Avg Power', 'Max Power']
         self.X = dataframe.loc[:, dataframe.columns != 'TSS']
-        self.y = dataframe['TSS']
+        self.y = dataframe['Training Load Indicator']
         self.random_state = np.random.RandomState(0)
 
     def _split_train_validation(self):
@@ -53,8 +63,17 @@ class PerformanceModelBuilder():
 
 
 def process_train_load_modeling(athletes_name):
-    pass
+    loader = data_loader.DataLoader()
+    data_set = loader.load_merged_data(athletes_name)
+    data_set_modeling = data_set[data_set['Training Stress Score®'].notnull()]
+    print(data_set_modeling)
 
 
 def process_performance_modeling(athletes_name):
     pass
+
+
+if __name__ == '__main__':
+    athletes_names = ['eduardo oliveira']
+    process_train_load_modeling(athletes_names[0])
+    process_performance_modeling(athletes_names[0])
