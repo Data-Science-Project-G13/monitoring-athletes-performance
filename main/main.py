b699a48f7cf7dfff14580ae9894cad548e953927
qiutingli <qiutingli.china@gmail.com>
2020-09-25 13:58:11 +1000

    Update with multiple athletes

--- main/main.py
+++ main/main.py
@@ -18,23 +18,24 @@ def main(athletes_name):
     """ Run the system
     Process: Data cleaning, Feature Engineering, Modeling.
     """
+    athletes_name = athletes_name.lower()
 
     # Data Cleaning
     data_cleaning.main('spreadsheet', athletes_name)
-    activity_types, split_type = ['cycling', 'running', 'swimming'], 'real-time'
-    for activity_type in activity_types:
+    fit_activity_types, split_type = ['cycling', 'running', 'swimming'], 'real-time'
+    for activity_type in fit_activity_types:
         data_cleaning.main('additional', athletes_name, activity_type=activity_type, split_type=split_type)
 
     # Feature Engineering
     data_merge.merge_spreadsheet_additional(athletes_name)
 
-    # Modeling
-    data_modelling.process_train_load_modeling(athletes_name)
+    # # Modeling
+    # data_modelling.process_train_load_modeling(athletes_name)
 
 
 if __name__ == '__main__':
-    system_initialize.initialize_system()
-    athletes_names = ['Eduardo Oliveira']   # 'Simon R Gronow'
+    athletes_names = ['Eduardo Oliveira', 'Xu Chen', 'Carly Hart']
+    system_initialize.initialize_system(athletes_names)
     for athletes_name in athletes_names:
         main(athletes_name)
 
