6631846e04b03958ce84fc2632a31411f072a70b
qiutingli <qiutingli.china@gmail.com>
2020-09-27 23:39:35 +1000

    Add comments

--- main/data_loader.py
+++ main/data_loader.py
@@ -7,11 +7,9 @@ This tool accepts comma separated value files (.csv).
 This script requires that `pandas` be installed within the Python
 environment you are running this script in.
 
-This file can also be imported as a module and contains the following
-functions:
+This file can also be imported as a module and contains the following class:
 
-    * load_athlete_dataframe - returns a pandas data_frame
-    * main - the main function of the script
+    * DataLoader - supports loading all data files for the system.
 """
 
 import os
