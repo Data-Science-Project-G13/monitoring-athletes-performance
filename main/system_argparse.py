11bb2c273b1ac31a2541b3ca4180357dd364d445
qiutingli <qiutingli.china@gmail.com>
2020-09-30 17:03:33 +1000

    Add system argparse

---
+++ main/system_argparse.py
@@ -0,0 +1,35 @@
+import os
+import argparse
+
+def parse_options(internal_args: []):
+    parser = argparse.ArgumentParser(description='Run System')
+    parser.add_argument('--initialize-system', dest='do_initialize', required=False,
+                        type=lambda x: (str(x).lower() == 'true'),
+                        default=True, help='whether initialize the system')
+    parser.add_argument('--clean-data', dest='do_cleaning', required=False,
+                        type=lambda x: (str(x).lower() == 'true'),
+                        default=True, help='whether clean the athlete data')
+    parser.add_argument('--process-feature-engineering', dest='do_feature_engineering', required=False,
+                        type=lambda x: (str(x).lower() == 'true'),
+                        default=True, help='whether process feature engineering')
+    parser.add_argument('--build-model', dest='do_modeling', required=False,
+                        type=lambda x: (str(x).lower() == 'true'),
+                        default=True, help='whether process modeling')
+    parser.add_argument('--generate-pmc', dest='do_pmc_generating', required=False,
+                        type=lambda x: (str(x).lower() == 'true'),
+                        default=True, help='whether generate pmc')
+    parser.add_argument('--athletes-names', nargs='+', dest='athletes_names', required=True,
+                        help='whether generate pmc')
+    if internal_args:
+        options = vars(parser.parse_args(internal_args))
+        options['athletes_names'] = list(filter(lambda x:
+                                                '--athletes-names' in x, internal_args))[0].split('=')[1].split()
+    else:
+        options = vars(parser.parse_args())
+    return options
+
+
+if __name__ == '__main__':
+    # print(parse_options([]))
+    # python system_argparse.py --athletes-names xuchen eddie --initialize-system=False
+    print(parse_options(["--initialize-system=False", "--athletes-names=xu_chen eduardo_oliveira carly_hart"]))
