8f26d2f38d83db550f125c03b19600a63be70bae
qiutingli <qiutingli.china@gmail.com>
2020-09-30 14:45:43 +1000

    Complete PMC generate

--- main/generate_pmc.py
+++ main/generate_pmc.py
@@ -2,14 +2,21 @@
 import os
 import numpy as np
 import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 # Self-defined modules
 import utility
 from data_loader import DataLoader
 
 
-def _add_fitness_fatigue(spreadsheet):
-    spreadsheet['ATL'] = spreadsheet.rolling('7d', min_periods=1, on='Date')['Training Stress Score®'].mean()
-    spreadsheet['CTL'] = spreadsheet.rolling('42d', min_periods=1, on='Date')['Training Stress Score®'].mean()
+def _add_fitness_fatigue(dataframe):
+    dataframe['Date'] = pd.to_datetime(dataframe.Date)
+    dataframe['Fatigue (ATL)'] = dataframe.rolling('7d', min_periods=1, on='Date')['Training Stress Score®'].mean()
+    dataframe['Fitness (CTL)'] = dataframe.rolling('42d', min_periods=1, on='Date')['Training Stress Score®'].mean()
+
+
+def _add_form(dataframe):
+    dataframe['Form (TSB)'] = dataframe['Fitness (CTL)'] - dataframe['Fatigue (ATL)']
 
 
 def _label_data_record(spreadsheet):
@@ -28,12 +35,58 @@ def _label_data_record(spreadsheet):
     spreadsheet['Training Load Indicator'] = pd.Series(indicators, index=spreadsheet.index)
 
 
+def _generate_pmc(athletes_name, dataframe, save=True):
+    fig, ax = plt.subplots()
+    ax2 = ax.twinx()
+    dates = dataframe['Date']
+    TSS, ATL, CTL, TSB = 'Training Stress Score®', 'Fatigue (ATL)', 'Fitness (CTL)', 'Form (TSB)'
+
+    def plot_TSS():
+        tss = dataframe[TSS]
+        ax.plot(dates, tss, '.', label='TSS', color='lightskyblue')
+        ax.legend(loc=2)
+        # rotate and align the tick labels so they look better
+        fig.autofmt_xdate()
+        ax.yaxis.set_label_position("left")
+        # ax.set_xlabel('Dates')
+        ax.set_ylabel('TSS')
+        ax.set_ylim([50, 250])
+
+    def plot_fatigue_and_fitness():
+        ax2.plot(dates, dataframe[ATL], label=ATL, color='green')
+        ax2.plot(dates, dataframe[CTL], label=CTL, color='orange')
+        ax2.plot(dates, dataframe[TSB], label=TSB, color='pink')
+        ax2.set_ylabel('CTL / ATL / TSB')
+        ax2.set_ylim([-100, 275])
+
+    def plot_zones():
+        year, month, day = str(dates.iloc[0]).split(' ')[0].split('-')
+        end_date = datetime.date(int(year),int(month), int(day))
+        font_size = 9
+        line_params = [(-30, 'red'), (-10, 'green'), (5, 'grey'), (25, 'royalblue')]
+        text_params = [(-42, 'red', 'High Risk Zone'), (-22, 'green', 'Optimal Training Zone'),
+                       (-7, 'grey', 'Grey Zone'), (13, 'royalblue', 'Freshness Zone'),
+                       (30, 'darkgoldenrod', 'Transition Zone')]
+        for params in line_params:
+            plt.axhline(xmin=0.05, xmax=1, y=params[0], color=params[1], linestyle='-')
+        for params in text_params:
+            plt.text(x=end_date, y=params[0], s=params[2], horizontalalignment='right',
+                     color=params[1], fontsize=font_size)
+    plot_TSS()
+    plot_fatigue_and_fitness()
+    plot_zones()
+
+    plt.title('Performance Management Chart - {}'.format(athletes_name.title()))
+    plt.legend()
+    plt.savefig('{}/plots/PMC - {}.jpg'.format(os.path.pardir, athletes_name.title()),
+                format='jpg', dpi=1200) if save else plt.show()
+
+
 def get_tss_estimated_data(athletes_name):
     TSS = 'Training Stress Score®'
     original_merged_data = DataLoader().load_merged_data(athletes_name=athletes_name)
     null_tss_data = original_merged_data[~original_merged_data[TSS].notnull()]
     sub_dataframe_dict = utility.split_dataframe_by_activities(original_merged_data)
-    print(list(original_merged_data[TSS]))
     model_types = utility.get_train_load_model_types(athletes_name)
     for activity, sub_dataframe in sub_dataframe_dict.items():
         best_model_type_for_activity = model_types[activity]
@@ -54,10 +107,14 @@ def get_tss_estimated_data(athletes_name):
     return original_merged_data
 
 
-def process_pmc_generation(athletes_name):
-    data = get_tss_estimated_data(athletes_name)
+def process_pmc_generation(athletes_name, save_pmc_figure=True):
+    dataframe_for_pmc = get_tss_estimated_data(athletes_name)
+    _add_fitness_fatigue(dataframe_for_pmc)
+    _add_form(dataframe_for_pmc)
+    _generate_pmc(athletes_name, dataframe_for_pmc, save=save_pmc_figure)
 
 
 if __name__ == '__main__':
-    athletes_name = 'xu chen'
-    process_pmc_generation(athletes_name)
+    athletes_names = ['eduardo oliveira', 'xu chen', 'carly hart']
+    for athletes_name in athletes_names:
+        process_pmc_generation(athletes_name)
